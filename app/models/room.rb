aff968022ce38f5fc56577a8ed5f81bfbdbdbf5b
agkazuya <k.agata0088@gmail.com>
2025-02-05 21:07:00 +0900

    チャットルーム作成機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+
+  validates :name, presence: true
+# 上記のバリデーションは「ルーム名が存在（presence）している場合のみ作成可（true）」という意味です。
 end
